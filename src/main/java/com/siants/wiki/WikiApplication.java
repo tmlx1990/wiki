1288dc891e9e09ad068a153d2c4b250bb14c1e74
tmlx1990 <tmlx1990@163.com>
2022-10-21 23:05:05 +0800

    2-5修改启动文案

--- src/main/java/com/siants/wiki/WikiApplication.java
+++ src/main/java/com/siants/wiki/WikiApplication.java
@@ -1,13 +1,20 @@
 package com.siants.wiki;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@Slf4j
 public class WikiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("启动成功！！");
+        log.info("地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
     }
 
 }
