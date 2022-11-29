6dd6941a63c90c5102607ca971284c8932512cad
tmlx1990 <tmlx1990@163.com>
2022-11-29 17:50:08 +0800

    3-5 增加demo表列表查询

---
+++ src/main/java/com/siants/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.siants.wiki.service;
+
+import com.siants.wiki.domain.Demo;
+import com.siants.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+}
