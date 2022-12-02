ef5a96e40fe48ddf6a405e2930cbd08531daf876
tmlx1990 <tmlx1990@163.com>
2022-12-02 23:56:34 +0800

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/siants/wiki/service/EbookService.java
+++ src/main/java/com/siants/wiki/service/EbookService.java
@@ -5,6 +5,7 @@ import com.siants.wiki.domain.EbookExample;
 import com.siants.wiki.mapper.EbookMapper;
 import com.siants.wiki.req.EbookReq;
 import com.siants.wiki.resp.EbookResp;
+import com.siants.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -24,12 +25,16 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
+        /*List<EbookResp> respList = new ArrayList<>();
         for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
+//            EbookResp ebookResp = new EbookResp();
+//            BeanUtils.copyProperties(ebook, ebookResp);
+            // 对象复制
+            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
             respList.add(ebookResp);
-        }
-        return respList;
+        }*/
+        // 列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 }
