fceeb03a29e625ef087d8cf67c4666c2c09ae123
tmlx1990 <tmlx1990@163.com>
2022-12-02 23:35:20 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/siants/wiki/controller/EbookController.java
+++ src/main/java/com/siants/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.siants.wiki.controller;
 
-import com.siants.wiki.domain.Ebook;
+import com.siants.wiki.req.EbookReq;
 import com.siants.wiki.resp.CommonResp;
+import com.siants.wiki.resp.EbookResp;
 import com.siants.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
---
+++ src/main/java/com/siants/wiki/req/EbookReq.java
@@ -0,0 +1,24 @@
+package com.siants.wiki.req;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EbookReq {
+    private Long id;
+
+    private String name;
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append("]");
+        return sb.toString();
+    }
+}
