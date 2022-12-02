885bad652a13375aa15cf3e2180ea335462def89
tmlx1990 <tmlx1990@163.com>
2022-12-02 17:44:48 +0800

    3-6 完成电子书列表查询接口

---
+++ src/main/java/com/siants/wiki/controller/EbookController.java
@@ -0,0 +1,25 @@
+package com.siants.wiki.controller;
+
+import com.siants.wiki.domain.Ebook;
+import com.siants.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public List<Ebook> list() {
+
+        return ebookService.list();
+    }
+
+}
---
+++ src/main/java/com/siants/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.siants.wiki.service;
+
+import com.siants.wiki.domain.Ebook;
+import com.siants.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
+    }
+}
