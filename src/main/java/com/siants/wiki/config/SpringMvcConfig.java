0b160d96e104bdb068023894446d22234eac30d5
tmlx1990 <tmlx1990@163.com>
2022-12-06 11:10:39 +0800

    5-8 增加拦截器，打印接口耗时

---
+++ src/main/java/com/siants/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,21 @@
+package com.siants.wiki.config;
+
+import com.siants.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**");
+    }
+}
