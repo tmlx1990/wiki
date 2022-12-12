d043b20c0236f8ddbdfad6904ce20cec3f3a7240
tmlx1990 <tmlx1990@163.com>
2022-12-13 00:22:08 +0800

    6-6 首页显示电子书列表，使用固定分页参数，一次性查出所有电子书

--- web/src/views/homeView.vue
+++ web/src/views/homeView.vue
@@ -72,19 +72,19 @@ import {defineComponent, onMounted, reactive, ref, toRef} from 'vue';
 //import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
 import axios from "axios";
 
-const listData: Record<string, string>[] = [];
-
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://joeschmoe.io/api/v1/random',
-    description:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
+// const listData: Record<string, string>[] = [];
+//
+// for (let i = 0; i < 23; i++) {
+//   listData.push({
+//     href: 'https://www.antdv.com/',
+//     title: `ant design vue part ${i}`,
+//     avatar: 'https://joeschmoe.io/api/v1/random',
+//     description:
+//         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+//     content:
+//         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+//   });
+// }
 
 
 export default defineComponent({
@@ -100,10 +100,15 @@ export default defineComponent({
     const ebooks1 = reactive({books: []});
 
     onMounted(() =>{
-      axios.get("/ebook/list").then((response) => {
+      axios.get("/ebook/list", {
+        params: {
+          page: 1,
+          size: 1000
+        }
+      }).then((response) => {
         const data = response.data;
-        ebooks.value = data.content;
-        ebooks1.books = data.content;
+        ebooks.value = data.content.list;
+        //ebooks1.books = data.content;
       })
     });
     const pagination = {
@@ -119,8 +124,8 @@ export default defineComponent({
     ];
     return {
       ebooks,
-      ebooks2: toRef(ebooks1, "books"),
-      listData,
+      //ebooks2: toRef(ebooks1, "books"),
+      //listData,
       pagination,
       actions,
     }
