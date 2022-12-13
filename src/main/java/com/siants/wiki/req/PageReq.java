3ecdd9a42b45b1b6a1b98bbee89afd0319338c0c
tmlx1990 <tmlx1990@163.com>
2022-12-13 18:17:01 +0800

    6-11 集成Validation，为列表接口增加参数校验

--- src/main/java/com/siants/wiki/req/PageReq.java
+++ src/main/java/com/siants/wiki/req/PageReq.java
@@ -3,12 +3,17 @@ package com.siants.wiki.req;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class PageReq {
-
+    @NotNull(message = "【页码】不能为空")
     private int page;
 
+    @NotNull(message = "【每页条数】不能为空")
+    @Max(value = 1000, message = "【每页条数】不能超过1000")
     private int size;
 
     @Override
