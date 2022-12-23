2a3c3a2b12502bae7aee74d5fc907f70dbd4aa12
tmlx1990 <tmlx1990@163.com>
2022-12-23 17:07:06 +0800

    7-7 首页显示分类菜单

--- web/src/views/homeView.vue
+++ web/src/views/homeView.vue
@@ -5,43 +5,28 @@
           v-model:openKeys="openKeys"
           mode="inline"
           style="height: 100%"
+          @click="handleClick"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-                <span>
-                  <user-outlined />
-                  subnav 1
-                </span>
+        <a-menu-item key="welcome">
+          <router-link :to="'/'">
+            <MailOutlined />
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
+              <span>
+                <user-outlined />
+                {{item.name}}
+              </span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined /><span>{{child.name}}</span>
+          </a-menu-item>
         </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-                <span>
-                  <laptop-outlined />
-                  subnav 2
-                </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-                <span>
-                  <notification-outlined />
-                  subnav 3
-                </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
-        </a-sub-menu>
+        <a-menu-item key="tip" :disabled="true">
+          <span>以上菜单在分类管理配置</span>
+        </a-menu-item>
       </a-menu>
     </a-layout-sider>
     <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
@@ -71,6 +56,8 @@
 import {defineComponent, onMounted, reactive, ref, toRef} from 'vue';
 //import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
 import axios from "axios";
+import {Tool} from "@/util/tool";
+import {message} from "ant-design-vue";
 
 // const listData: Record<string, string>[] = [];
 //
@@ -99,7 +86,34 @@ export default defineComponent({
     const ebooks = ref();
     const ebooks1 = reactive({books: []});
 
+    const level1 = ref();
+    let categorys: any;
+    /**
+     * 查询所有分类
+     */
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组：", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("属性结构：", level1);
+        } else {
+          message.error(data.message);
+        }
+
+      });
+    };
+
+    const handleClick = () => {
+      console.log("menu click")
+    };
+
     onMounted(() =>{
+      handleQueryCategory();
       axios.get("/ebook/list", {
         params: {
           page: 1,
@@ -128,6 +142,8 @@ export default defineComponent({
       //listData,
       pagination,
       actions,
+      level1,
+      handleClick,
     }
   }
 });
