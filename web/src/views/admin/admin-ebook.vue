f278ffdc2b5ee75a82d1e470d198ce0393d20f94
tmlx1990 <tmlx1990@163.com>
2022-12-23 12:32:12 +0800

    7-6 电子书列表显示分类名称

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -43,6 +43,9 @@
         <template #cover="{ text: cover}">
           <img v-if="cover" :src="cover" alt="avatar" />
         </template>
+        <template v-slot:category="{ text, record }">
+          <span>{{ getCategoryName(record.category1Id) }} / {{ getCategoryName(record.category2Id) }}</span>
+        </template>
         <template v-slot:action="{ text, record }">
           <a-space size="small">
             <a-button type="primary" @click="edit(record)">
@@ -123,16 +126,8 @@ export default defineComponent({
         dataIndex: 'name'
       },
       {
-        title: '分类一',
-        key: 'category1Id',
-        dataIndex: 'category1Id',
-        slots: { customRender: 'name'}
-      },
-      {
-        title: '分类二',
-        key: 'category2Id',
-        dataIndex: 'category2Id',
-        slots: { customRender: 'name'}
+        title: '分类',
+        slots: { customRender: 'category'}
       },
       {
         title: '文档数',
@@ -251,6 +246,7 @@ export default defineComponent({
     };
 
     const level1 = ref();
+    let categorys: any;
     /**
      * 查询所有分类
      */
@@ -260,7 +256,7 @@ export default defineComponent({
         loading.value = false;
         const data = response.data;
         if (data.success) {
-          const categorys = data.content;
+          categorys = data.content;
           console.log("原始数组：", categorys);
 
           level1.value = [];
@@ -271,6 +267,18 @@ export default defineComponent({
         }
 
       });
+    };
+
+    const getCategoryName = (cid: number) => {
+      // console.log(cid)
+      let result = "";
+      categorys.forEach((item: any) => {
+        if (item.id === cid) {
+          // return item.name; // 注意，这里直接return不起作用
+          result = item.name;
+        }
+      });
+      return result;
     };
 
     onMounted(() => {
@@ -287,6 +295,7 @@ export default defineComponent({
       columns,
       loading,
       handleTableChange,
+      getCategoryName,
 
       edit,
       add,
