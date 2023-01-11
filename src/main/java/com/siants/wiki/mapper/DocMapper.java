b8fb1ee6f9348b4dddcd6c536a59909653b415f7
tmlx1990 <tmlx1990@163.com>
2023-01-11 11:30:30 +0800

    8-2 增加文档表SQL，并生成持久层代码

---
+++ src/main/java/com/siants/wiki/mapper/DocMapper.java
@@ -0,0 +1,30 @@
+package com.siants.wiki.mapper;
+
+import com.siants.wiki.domain.Doc;
+import com.siants.wiki.domain.DocExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DocMapper {
+    long countByExample(DocExample example);
+
+    int deleteByExample(DocExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Doc row);
+
+    int insertSelective(Doc row);
+
+    List<Doc> selectByExample(DocExample example);
+
+    Doc selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") Doc row, @Param("example") DocExample example);
+
+    int updateByExample(@Param("row") Doc row, @Param("example") DocExample example);
+
+    int updateByPrimaryKeySelective(Doc row);
+
+    int updateByPrimaryKey(Doc row);
+}
