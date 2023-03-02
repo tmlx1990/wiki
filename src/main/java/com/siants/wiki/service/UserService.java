004e59ba6661280a1ef138393d6088ae2bd53b86
tmlx1990 <tmlx1990@163.com>
2023-03-02 15:50:39 +0800

    9-4 增加校验用户名不能重复，增加自定义异常

--- src/main/java/com/siants/wiki/service/UserService.java
+++ src/main/java/com/siants/wiki/service/UserService.java
@@ -4,6 +4,8 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.siants.wiki.domain.User;
 import com.siants.wiki.domain.UserExample;
+import com.siants.wiki.exception.BusinessException;
+import com.siants.wiki.exception.BusinessExceptionCode;
 import com.siants.wiki.mapper.UserMapper;
 import com.siants.wiki.req.UserQueryReq;
 import com.siants.wiki.req.UserSaveReq;
@@ -13,6 +15,7 @@ import com.siants.wiki.util.CopyUtil;
 import com.siants.wiki.util.SnowFlake;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -65,9 +68,15 @@ public class UserService {
     public void save(UserSaveReq req) {
         User user = CopyUtil.copy(req, User.class);
         if (ObjectUtils.isEmpty(req.getId())) {
-            // 新增
-            user.setId(snowFlake.nextId());
-            userMapper.insert(user);
+            User userDB = selectByLoginName(req.getLoginName());
+            if (ObjectUtils.isEmpty(userDB)) {
+                // 新增
+                user.setId(snowFlake.nextId());
+                userMapper.insert(user);
+            } else {
+                // 用户名已存在
+                throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+            }
         } else {
             // 更新
             userMapper.updateByPrimaryKey(user);
@@ -76,5 +85,17 @@ public class UserService {
 
     public void delete(Long id) {
         userMapper.deleteByPrimaryKey(id);
+    }
+
+    public User selectByLoginName(String loginName) {
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        criteria.andLoginNameEqualTo(loginName);
+        List<User> userList = userMapper.selectByExample(userExample);
+        if (CollectionUtils.isEmpty(userList)) {
+            return null;
+        } else {
+            return userList.get(0);
+        }
     }
 }
