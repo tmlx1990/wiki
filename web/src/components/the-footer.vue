8041032b029afdd7fc461ac819ff65772a2859e0
tmlx1990 <tmlx1990@163.com>
2023-03-09 16:08:26 +0800

    9-9 vuex整合sessionStorage，解决刷新数据丢失问题

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout-footer style="text-align: center">
-    欢迎：{{user.name}}
+    <span v-show="user.id">欢迎：{{user.name}}</span>
     Ant Design ©2022 Created by Ant UED
   </a-layout-footer>
 </template>
