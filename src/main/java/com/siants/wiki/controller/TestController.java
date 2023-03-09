e6425f5339b3d5fca8f6bed04af6053ead1cf9b6
tmlx1990 <tmlx1990@163.com>
2023-03-09 14:05:18 +0800

    9-9 增加redis测试接口

--- src/main/java/com/siants/wiki/controller/TestController.java
+++ src/main/java/com/siants/wiki/controller/TestController.java
@@ -2,15 +2,17 @@ package com.siants.wiki.controller;
 
 import com.siants.wiki.domain.Test;
 import com.siants.wiki.service.TestService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
+@Slf4j
 public class TestController {
 
     @Value("${test.hello:TEST}")
@@ -18,6 +20,9 @@ public class TestController {
 
     @Resource
     private TestService testService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     /**
      * GET, POST, PUT, DELETE
@@ -47,4 +52,17 @@ public class TestController {
         return testService.list();
     }
 
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        log.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        log.info("key: {}, value: {}", key, object);
+        return object;
+    }
 }
