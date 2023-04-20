1c75b66f1ea5214d06cd6f0f88e0765434bf6898
tmlx1990 <tmlx1990@163.com>
2023-04-20 14:13:51 +0800

    10-10 修复后台传递id精度丢失问题

--- src/main/java/com/siants/wiki/resp/DocQueryResp.java
+++ src/main/java/com/siants/wiki/resp/DocQueryResp.java
@@ -1,6 +1,11 @@
 package com.siants.wiki.resp;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
 public class DocQueryResp {
+
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
     private Long ebookId;
